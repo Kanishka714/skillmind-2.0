bf6adb76f692d089cd2d9be3ca438b9e0eaf853e
kanishka714 <kanishkadilshan861@gmail.com>
2025-04-26 11:05:01 +0530

    initialized backend

---
+++ backend/src/test/java/com/skillmind/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillmind.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
